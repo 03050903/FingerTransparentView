465ef99018f62552f9b03e3a6534c6b3c723ffd9
drakeet <drakeet.me@gmail.com>
2015-10-04 20:33:57 +0800

    QAQ _(:з」∠)_ hand revert

--- library/src/main/java/me/drakeet/fingertransparentview/FingerTransparentView.java
+++ library/src/main/java/me/drakeet/fingertransparentview/FingerTransparentView.java
@@ -1,16 +1,22 @@
 package me.drakeet.fingertransparentview;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Build;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.Xfermode;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.Date;
-
 /**
- * https://github.com/drakeet/BreathingViewHelper
- *
- * Created by drakeet on 6/12/15.
+ * Created by drakeet on 7/29/15.
  */
 public class FingerTransparentView extends View {
 
@@ -27,23 +33,26 @@ public class FingerTransparentView extends View {
     private boolean mCanScale = true;
     private Xfermode mXfermode;
 
+
     public FingerTransparentView(Context context) {
         super(context);
     }
+
 
     public FingerTransparentView(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FingerTransparentView);
 
         try {
-            mFingerRadius = a.getDimensionPixelSize(
-                    R.styleable.FingerTransparentView_transparent_radius,
-                    getResources().getDimensionPixelSize(R.dimen.finger_transparent_radius_default)
-            );
+            mFingerRadius =
+                    a.getDimensionPixelSize(R.styleable.FingerTransparentView_transparent_radius,
+                            getResources().getDimensionPixelSize(
+                                    R.dimen.finger_transparent_radius_default));
         } finally {
             a.recycle();
         }
     }
+
 
     private void init() {
         mShowBelowViewRect = new Rect();
@@ -63,8 +72,7 @@ public class FingerTransparentView extends View {
         setWillNotDraw(false);
 
         mZoomTouchListener = new OnScaleTouchListener() {
-            @Override
-            public void onScale(float scale) {
+            @Override public void onScale(float scale) {
                 setScale(scale);
             }
         };
@@ -72,10 +80,12 @@ public class FingerTransparentView extends View {
         if (mCanScale) this.setOnTouchListener(mZoomTouchListener);
     }
 
+
     private void initFingerLayer() {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.finger);
         mFingerLayer = scaleBitmap(bitmap, mScale);
     }
+
 
     private void initBaseLayer() {
         mBaseLayer = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
@@ -85,6 +95,7 @@ public class FingerTransparentView extends View {
         canvas.drawRect(mRect, mBasePaint);
     }
 
+
     private void resetBaseLayer() {
         Canvas canvas = new Canvas(mBaseLayer);
 
@@ -92,28 +103,29 @@ public class FingerTransparentView extends View {
         canvas.drawRect(mRect, mBasePaint);
     }
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
+
+    @Override public boolean dispatchTouchEvent(MotionEvent event) {
         super.dispatchTouchEvent(event);
 
         if (mCanScale) {
             mZoomTouchListener.onTouch(this, event);
-        } else {
+        }
+        else {
             onTouchEvent(event);
         }
 
         return true;
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+
+    @Override public boolean onTouchEvent(MotionEvent event) {
         int action = event.getAction();
 
         int x = (int) event.getX();
         int y = (int) event.getY();
 
         mShowBelowViewRect.left = (int) (x - mFingerRadius * mScale / 2);
-        mShowBelowViewRect.right = (int) (x + mFingerRadius * mScale / 2 + 1) ;//加一防止小数点被砍掉出现漏缝
+        mShowBelowViewRect.right = (int) (x + mFingerRadius * mScale / 2 + 1);//加一防止小数点被砍掉出现漏缝
         mShowBelowViewRect.top = (int) (y - mFingerRadius * mScale);
         mShowBelowViewRect.bottom = y;
 
@@ -125,14 +137,15 @@ public class FingerTransparentView extends View {
             case MotionEvent.ACTION_POINTER_DOWN:
                 break;
             case MotionEvent.ACTION_DOWN:
-                mXfermode =  new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT);
+                mXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT);
                 // ↓↓↓
             default:
                 Canvas canvas = new Canvas();
                 canvas.setBitmap(mBaseLayer);
                 resetBaseLayer();
                 mTouchPaint.setXfermode(mXfermode);
-                canvas.drawBitmap(mFingerLayer, mShowBelowViewRect.left, mShowBelowViewRect.top, mTouchPaint);
+                canvas.drawBitmap(mFingerLayer, mShowBelowViewRect.left, mShowBelowViewRect.top,
+                        mTouchPaint);
                 mTouchPaint.setXfermode(null);
                 canvas.save();
         }
@@ -141,54 +154,51 @@ public class FingerTransparentView extends View {
         return true;
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
+
+    @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(mBaseLayer, null, mRect, null);
         canvas.drawBitmap(mFingerLayer, null, mShowBelowViewRect, null);
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+    @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth = w;
         mHeight = h;
         init();
     }
 
+
     /**
      * 等比缩放
      * add by Malin
-     *
-     * @param bitmap:资源图片
-     * @param with:手指缩放的半径
-     *
-     * @return
      */
     private Bitmap scaleBitmap(Bitmap bitmap, int with) {
-        return Bitmap.createScaledBitmap(
-                bitmap,
-                with,
-                with * bitmap.getWidth() / bitmap.getHeight(),
-                true
-        );
+        return Bitmap.createScaledBitmap(bitmap, with,
+                with * bitmap.getWidth() / bitmap.getHeight(), true);
     }
 
+
     private Bitmap scaleBitmap(Bitmap bitmap, float with) {
-        return scaleBitmap(bitmap, (int)(with * mFingerRadius));
+        return scaleBitmap(bitmap, (int) (with * mFingerRadius));
     }
+
 
     public int getFingerRadius() {
         return mFingerRadius;
     }
 
+
     public void setFingerRadius(int fingerRadius) {
         mFingerRadius = fingerRadius;
     }
 
+
     public float getScale() {
         return mScale;
     }
+
 
     public void setScale(float scale) {
         mScale = scale;
@@ -196,13 +206,14 @@ public class FingerTransparentView extends View {
         invalidate();
     }
 
+
     public boolean getCanScale() {
         return mCanScale;
     }
+
 
     public void setCanScale(boolean canScale) {
         mCanScale = canScale;
         if (mCanScale) this.setOnTouchListener(mZoomTouchListener);
     }
-
 }
